e4b9b76b339c94ab3b5080af66ffc30ea32a077d
Linuxperoxo <linuxperoxo@gmail.com>
2024-12-08 18:10:51 -0300

    gdt comments update

--- kernel/include/gdt.h
+++ kernel/include/gdt.h
@@ -6,7 +6,7 @@
  *    |  COPYRIGHT : (c) 2024 per Linuxperoxo.     |
  *    |  AUTHOR    : Linuxperoxo                   |
  *    |  FILE      : gdt.h                         |
- *    |  SRC MOD   : 06/12/2024                    |
+ *    |  SRC MOD   : 08/12/2024                    |
  *    |                                            |
  *    O--------------------------------------------/
  *
