e257b8e1b68a00fbfbe6d629a111fe0f5fb48999
Linuxperoxo <linuxperoxo@gmail.com>
2024-12-18 19:46:51 -0300

    add shell to kernel

---
+++ kernel/include/shell.h
@@ -0,0 +1,19 @@
+/*
+ *
+ *
+ *    /--------------------------------------------O
+ *    |                                            |
+ *    |  COPYRIGHT : (c) 2024 per Linuxperoxo.     |
+ *    |  AUTHOR    : Linuxperoxo                   |
+ *    |  FILE      : shell.h                       |
+ *    |  SRC MOD   : 18/12/2024                    |
+ *    |                                            |
+ *    O--------------------------------------------/
+ *
+ *
+ */
+
+#ifndef __K_SHELL__
+#define __K_SHELL__
+
+#endif
