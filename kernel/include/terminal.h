1217d9f7adf3d5ea839106aff9167e9feebd0854
Linuxperoxo <linuxperoxo@gmail.com>
2024-12-21 12:31:02 -0300

    update kernel terminal

--- kernel/include/terminal.h
+++ kernel/include/terminal.h
@@ -6,7 +6,7 @@
  *    |  COPYRIGHT : (c) 2024 per Linuxperoxo.     |
  *    |  AUTHOR    : Linuxperoxo                   |
  *    |  FILE      : terminal.h                    |
- *    |  SRC MOD   : 20/12/2024                    | 
+ *    |  SRC MOD   : 21/12/2024                    | 
  *    |                                            |
  *    O--------------------------------------------/
  *    
@@ -17,30 +17,11 @@
 #define __K_TERMINAL__
 
 #include <std/int.h>
+#include <video/vga/vga.h>
 
-#define MAX_IN_BUFFER_SIZE 512
-#define MAX_OUT_BUFFER_SIZE 512
+#define MAX_IN_BUFFER_SIZE DEFAULT_WIDTH * DEFAULT_WIDTH
+#define MAX_OUT_BUFFER_SIZE DEFAULT_HEIGHT * DEFAULT_WIDTH
 #define MAX_ERR_BUFFER_SIZE 256
-
-struct VGAState;
-struct Keyboard;
-
-struct Terminal
-{
-  struct VGAState* __this_section_vga_state;
-  struct Keyboard* __this_section_keyboard;
-  
-  __u8 __terminal_in_buffer[MAX_IN_BUFFER_SIZE];
-  __u16 __in_buffer_offset;
-
-  __u8 __terminal_out_buffer[MAX_OUT_BUFFER_SIZE];
-  __u16 __out_buffer_offset;
-
-  __u8 __terminal_err_buffer[MAX_ERR_BUFFER_SIZE];
-  __u16 __err_buffer_offset;
-};
-
-extern struct Terminal __current_section;
 
 extern void terminal_init();
 extern void terminal_init_vga();
@@ -48,5 +29,6 @@ extern void terminal_out_write(const char);
 extern void terminal_output_clear();
 extern void terminal_input_clear();
 extern __u8 terminal_get_last_key();
+extern void terminal_cpy_input(char*, __u32);
 
 #endif
