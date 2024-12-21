3cac237f2eedf42b29c0daffbb9dd7e7f7baf3a9
Linuxperoxo <linuxperoxo@gmail.com>
2024-12-21 12:25:12 -0300

    add little shell for testing :)

--- kernel/include/shell.h
+++ kernel/include/shell.h
@@ -6,7 +6,7 @@
  *    |  COPYRIGHT : (c) 2024 per Linuxperoxo.     |
  *    |  AUTHOR    : Linuxperoxo                   |
  *    |  FILE      : shell.h                       |
- *    |  SRC MOD   : 18/12/2024                    |
+ *    |  SRC MOD   : 21/12/2024                    |
  *    |                                            |
  *    O--------------------------------------------/
  *
@@ -15,5 +15,12 @@
 
 #ifndef __K_SHELL__
 #define __K_SHELL__
+
+#include <terminal.h>
+
+#define SHELL_PS1 "[ROOT]$ "
+#define SHELL_IN_BUFFER_SIZE MAX_IN_BUFFER_SIZE
+
+void shell_init();
 
 #endif
--- kernel/src/shell.c
+++ kernel/src/shell.c
@@ -1,1 +1,36 @@
+/*
+ *
+ *
+ *    /--------------------------------------------O
+ *    |                                            |
+ *    |  COPYRIGHT : (c) 2024 per Linuxperoxo.     |
+ *    |  AUTHOR    : Linuxperoxo                   |
+ *    |  FILE      : shell.c                       |
+ *    |  SRC MOD   : 21/12/2024                    |
+ *    |                                            |
+ *    O--------------------------------------------/
+ *
+ *
+ */
+
 #include <shell.h>
+#include <std/str.h>
+#include <std/io.h>
+#include <terminal.h>
+
+struct Shell
+{
+  char __buffer[SHELL_IN_BUFFER_SIZE];
+};
+
+struct Shell __shell;
+
+void shell_init()
+{
+  while(1)
+  {
+    printf((char*)SHELL_PS1);
+    scanf((char*)&__shell.__buffer, SHELL_IN_BUFFER_SIZE);
+  }
+}
+
