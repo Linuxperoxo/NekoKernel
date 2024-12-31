ff179d088e65065d3f7e02614092111f7f25f008
Linuxperoxo <linuxperoxo@gmail.com>
2024-12-31 02:26:02 -0300

    update terminal

--- kernel/src/terminal.c
+++ kernel/src/terminal.c
@@ -6,7 +6,7 @@
  *    |  COPYRIGHT : (c) 2024 per Linuxperoxo.     |
  *    |  AUTHOR    : Linuxperoxo                   |
  *    |  FILE      : terminal.c                    |
- *    |  SRC MOD   : 21/12/2024                    | 
+ *    |  SRC MOD   : 30/12/2024                    | 
  *    |                                            |
  *    O--------------------------------------------/
  *    
@@ -21,22 +21,37 @@
 #include <sys/ports.h>
 #include <device/io/keyboard/keyboard.h>
 
-struct Terminal
+/*
+ *
+ * Macros
+ *
+ */
+
+#ifndef NULL
+#define NULL (void*)0x00
+#endif
+
+#define TERMINAL __terminal
+#define TERMINAL_OUT_OFFSET __terminal->__out_offset
+#define TERMINAL_IN_OFFSET __terminal->__in_offset
+#define TERMINAL_VGA __terminal->__vga_state
+
+struct Terminal* __terminal = NULL;
+
+/*
+ *
+ * Internal Functions
+ *
+ */
+
+__attribute__((always_inline)) inline void terminal_put_char(const char __ch__)
 {
-  struct VGAState* __this_section_vga_state;
-  struct Keyboard* __this_section_keyboard;
+  TERMINAL_VGA.__framebuffer[(DEFAULT_WIDTH * TERMINAL_VGA.__current_row * 2) + (TERMINAL_VGA.__current_col * 2)]     = __ch__;
+  TERMINAL_VGA.__framebuffer[(DEFAULT_WIDTH * TERMINAL_VGA.__current_row * 2) + (TERMINAL_VGA.__current_col * 2) + 1] = ((TERMINAL_VGA.__bc_color << 4) & 0x70) | (TERMINAL_VGA.__ch_color & 0x0F);
   
-  __u8 __terminal_in_buffer[MAX_IN_BUFFER_SIZE];
-  __u16 __in_buffer_offset;
-
-  __u8 __terminal_out_buffer[MAX_OUT_BUFFER_SIZE];
-  __u16 __out_buffer_offset;
-
-  __u8 __terminal_err_buffer[MAX_ERR_BUFFER_SIZE];
-  __u16 __err_buffer_offset;
-};
-
-struct Terminal __current_terminal;
+  TERMINAL_VGA.__last_put     = __ch__;
+  TERMINAL_VGA.__current_col += 0x01;
+}
 
 __attribute__((always_inline)) inline void terminal_mov_ptr(__u8 __row__, __u8 __col__)
 { 
@@ -48,7 +63,7 @@ __attribute__((always_inline)) inline void terminal_mov_ptr(__u8 __row__, __u8 _
 
   __u16 __offset = DEFAULT_WIDTH * __row__ + __col__;
 
- /*
+  /*
    *
    * O registrador 0x0E contém o byte mais significativo 
    * da posição do cursor (8 bits mais à esquerda).
@@ -75,149 +90,88 @@ __attribute__((always_inline)) inline void terminal_mov_ptr(__u8 __row__, __u8 _
   outb(VGA_DATA_PORT, __offset & 0xFF); // Envia o byte menos significativo da posição para o registrador de dados.
 }
 
-__attribute__((always_inline)) inline void terminal_in_write(const char __ch__)
-{
-  __current_terminal.__in_buffer_offset = __current_terminal.__in_buffer_offset >= MAX_IN_BUFFER_SIZE ? 0x00 : __current_terminal.__in_buffer_offset;
-    
-  if(__current_terminal.__this_section_vga_state->__current_col + 1 >= DEFAULT_WIDTH) {
-    __current_terminal.__this_section_vga_state->__current_row += 1;
-    __current_terminal.__this_section_vga_state->__current_col = 0;
-  }
+/*
+ *
+ * terminal.h
+ *
+ */
 
-  __current_terminal.__this_section_vga_state->__framebuffer[DEFAULT_WIDTH * __current_terminal.__this_section_vga_state->__current_row * 2 + __current_terminal.__this_section_vga_state->__current_col++ * 2] = __ch__;
-  __current_terminal.__this_section_vga_state->__last_put = __ch__;
-  __current_terminal.__terminal_in_buffer[__current_terminal.__in_buffer_offset++] = __current_terminal.__this_section_vga_state->__last_put;
-
-  terminal_mov_ptr(__current_terminal.__this_section_vga_state->__current_row, __current_terminal.__this_section_vga_state->__current_col);
-}
-
-__attribute__((always_inline)) inline void terminal_keyboard_print()
-{
-  switch(__current_terminal.__this_section_keyboard->__code)
+void terminal_init(struct Terminal* __terminal_section__)
+{ 
+  if(__terminal_section__ == NULL)
   {
-    case KEY_ENTER:
-      terminal_out_write('\n');
-    break;
-
-    case KEY_BACK:
-      if(__current_terminal.__in_buffer_offset > 0)
-      {
-        if(__current_terminal.__this_section_vga_state->__current_col == 0)
-        {
-          if(__current_terminal.__this_section_vga_state->__current_row > 0)
-          {
-            __current_terminal.__this_section_vga_state->__current_row -= 1;
-          }
-        } 
-        else 
-        {
-          __current_terminal.__this_section_vga_state->__current_col -= 1;
-        }
-
-        __current_terminal.__this_section_vga_state->__framebuffer[DEFAULT_WIDTH * __current_terminal.__this_section_vga_state->__current_row * 2 + __current_terminal.__this_section_vga_state->__current_col * 2] = '\0';
-        __current_terminal.__this_section_vga_state->__last_put = '\0';
-        __current_terminal.__in_buffer_offset -= __current_terminal.__in_buffer_offset == 0 ? 0 : 1;
-        __current_terminal.__terminal_in_buffer[__current_terminal.__in_buffer_offset] = '\0';
-        
-        terminal_mov_ptr(__current_terminal.__this_section_vga_state->__current_row, __current_terminal.__this_section_vga_state->__current_col);
-      }
-    break;
-
-    default:
-      if(KEY_IS_VISIBLE)
-        terminal_in_write(__current_terminal.__this_section_keyboard->__char);
-    break;
+    __asm__ volatile(
+      "int $0x11\n"
+      :
+      :
+      :
+    );
   }
-}
 
-void terminal_keyboard_input()
-{
-  if(KEY_IS_PRESS)
-    terminal_keyboard_print(__current_terminal.__this_section_keyboard->__char);
-}
+  /*
+   *
+   * Configurando struct terminal
+   *
+   */
 
-void terminal_init()
-{
-  __current_terminal.__in_buffer_offset = 0x00;
-  __current_terminal.__out_buffer_offset = 0x00;
-  __current_terminal.__err_buffer_offset = 0x00;
-  __current_terminal.__this_section_keyboard = &__keyboard;
-  __current_terminal.__this_section_keyboard->__flags |= 0b1000;
-}
-
-void terminal_init_vga()
-{
-  __current_terminal.__this_section_vga_state->__framebuffer = (__u8*) VGA_FRAMEBUFFER_ADDRS;
-  __current_terminal.__this_section_vga_state->__current_row = 0;
-  __current_terminal.__this_section_vga_state->__current_col = 0;
-  __current_terminal.__this_section_vga_state->__bc_color = DEFAULT_BC_COLOR;
-  __current_terminal.__this_section_vga_state->__ch_color = DEFAULT_CHAR_COLOR;
-}
-
-void terminal_out_write(const char __ch__)
-{
-  __current_terminal.__out_buffer_offset = __current_terminal.__out_buffer_offset >= MAX_IN_BUFFER_SIZE ? 0x00 : __current_terminal.__out_buffer_offset;
+  TERMINAL = __terminal_section__;
   
-  switch(__ch__) 
-  {
-    case '\n':
-      __current_terminal.__this_section_vga_state->__current_row += 1;
-      __current_terminal.__this_section_vga_state->__current_col = 0;   
-    break;
+  TERMINAL->__in_offset  = 0x00;
+  TERMINAL->__out_offset = 0x00;
+  
+  /*
+   *
+   * Configurando struct VGA linkada ao terminal
+   *
+   */
 
-    case '\r':
-      __current_terminal.__this_section_vga_state->__current_col = 0;
+  TERMINAL_VGA.__framebuffer = (void*)VGA_FRAMEBUFFER_ADDRS;
+  TERMINAL_VGA.__current_row = 0x00;
+  TERMINAL_VGA.__current_col = 0x00;
+  TERMINAL_VGA.__bc_color    = BLACK_BC_COLOR;
+  TERMINAL_VGA.__ch_color    = WHITE_CHAR_COLOR;
+  TERMINAL_VGA.__last_put    = 0x00;
+}
+
+void terminal_out(const char __ch__)
+{
+  TERMINAL->__out_buffer[TERMINAL_OUT_OFFSET] = __ch__;
+  
+  switch(TERMINAL->__out_buffer[TERMINAL_OUT_OFFSET])
+  {
+    case '\n':    
+      TERMINAL_VGA.__current_col  = 0x00;
+      TERMINAL_VGA.__current_row += 0x01;
+
+      ++TERMINAL_OUT_OFFSET;
+      
+      terminal_mov_ptr(TERMINAL_VGA.__current_row, TERMINAL_VGA.__current_col);
       return;
     break;
 
-    default:
-      if(__current_terminal.__this_section_vga_state->__current_col + 1 >= DEFAULT_WIDTH) {
-        __current_terminal.__this_section_vga_state->__current_row += 1;
-        __current_terminal.__this_section_vga_state->__current_col = 0;
-      }
+    case '\r': 
+      TERMINAL_VGA.__current_col = 0x00;
 
-      __current_terminal.__this_section_vga_state->__framebuffer[DEFAULT_WIDTH * __current_terminal.__this_section_vga_state->__current_row * 2 + __current_terminal.__this_section_vga_state->__current_col++ * 2] = __ch__;
-      __current_terminal.__this_section_vga_state->__last_put = __ch__;
-      __current_terminal.__terminal_out_buffer[__current_terminal.__out_buffer_offset++] = __current_terminal.__this_section_vga_state->__last_put;
+      ++TERMINAL_OUT_OFFSET;
+
+      terminal_mov_ptr(TERMINAL_VGA.__current_row, TERMINAL_VGA.__current_col);
+      return;
     break;
   }
-  terminal_mov_ptr(__current_terminal.__this_section_vga_state->__current_row, __current_terminal.__this_section_vga_state->__current_col);
-}
-
-void terminal_output_clear()
-{
-  __u16 __size = 0;
-  __u16 __screen_size = DEFAULT_HEIGHT * DEFAULT_WIDTH * 2;
-
-  while(__size < __screen_size) {
-    __current_terminal.__this_section_vga_state->__framebuffer[__size++] = 0x00;
-    __current_terminal.__this_section_vga_state->__framebuffer[__size++] = __current_terminal.__this_section_vga_state->__bc_color << 4 | __current_terminal.__this_section_vga_state->__ch_color;
+  
+  if(TERMINAL_VGA.__current_col >= DEFAULT_WIDTH)
+  {
+    TERMINAL_VGA.__current_col  = 0x00;
+    TERMINAL_VGA.__current_row += 0x01;
   }
-  __current_terminal.__this_section_vga_state->__last_put = 0x00;
-  __current_terminal.__out_buffer_offset = 0x00;
-  __current_terminal.__this_section_vga_state->__current_row = 0x00;
-  __current_terminal.__this_section_vga_state->__current_col = 0x00;
-
-  memset((char*)&__current_terminal.__out_buffer_offset, 0x00, MAX_OUT_BUFFER_SIZE);
-  terminal_mov_ptr(__current_terminal.__this_section_vga_state->__current_row, __current_terminal.__this_section_vga_state->__current_col);
+  terminal_put_char(TERMINAL->__out_buffer[TERMINAL_OUT_OFFSET++]);
+  terminal_mov_ptr(TERMINAL_VGA.__current_row, TERMINAL_VGA.__current_col);
 }
 
-void terminal_input_clear()
+void terminal_in(const char __ch__)
 {
-  __current_terminal.__this_section_keyboard->__code = 0x00;
-  __current_terminal.__this_section_keyboard->__scan = 0x00;
-  __current_terminal.__in_buffer_offset = 0x00;
-
-  memset((char*)&__current_terminal.__terminal_in_buffer, 0x00, MAX_IN_BUFFER_SIZE);
+  if(KEY_IS_PRESS && KEY_IS_VISIBLE)
+  {
+    terminal_out(__ch__);
+  }
 }
-
-__u8 terminal_get_last_key()
-{
-  return __current_terminal.__this_section_keyboard->__code;
-}
-
-void terminal_cpy_input(char* __dest__, __u32 __size__)
-{
-  memcpy(__dest__, &__current_terminal.__terminal_in_buffer, __size__);
-}
-
