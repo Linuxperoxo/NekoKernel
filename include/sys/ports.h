0e7476fdc2ffb91276fa83170088c967eae769b3
Linuxperoxo <linuxperoxo@gmail.com>
2025-01-08 17:44:39 -0300

    update ports.h

--- include/sys/ports.h
+++ include/sys/ports.h
@@ -6,7 +6,7 @@
  *    |  COPYRIGHT : (c) 2024 per Linuxperoxo.     |
  *    |  AUTHOR    : Linuxperoxo                   |
  *    |  FILE      : ports.h                       |
- *    |  SRC MOD   : 18/12/2024                    |
+ *    |  SRC MOD   : 08/01/2025                    |
  *    |                                            |
  *    O--------------------------------------------/
  *
@@ -43,7 +43,7 @@
 inline __attribute__((always_inline)) void outb(__u16 __port__, __u8 __value__)
 {
   __asm__ volatile(
-    "outb %%al, %%dx\n"
+    "outb %1, %0\n"
     :
     : "d"(__port__), "a"(__value__)
     :
@@ -53,7 +53,7 @@ inline __attribute__((always_inline)) void outb(__u16 __port__, __u8 __value__)
 inline __attribute__((always_inline)) void outw(__u16 __port__, __u16 __value__) 
 {
   __asm__ volatile(
-    "outw %%ax, %%dx\n"
+    "outw %1, %0\n"
     :
     : "d"(__port__), "a"(__value__)
     : 
@@ -63,7 +63,7 @@ inline __attribute__((always_inline)) void outw(__u16 __port__, __u16 __value__)
 inline __attribute__((always_inline)) void outl(__u16 __port__, __u32 __value__) 
 { 
   __asm__ volatile(
-    "outl %%eax, %%dx\n"
+    "outl %1, %0\n"
     :
     : "d"(__port__), "a"(__value__)
     :
@@ -75,7 +75,7 @@ inline __attribute__((always_inline)) __u8 inb(__u16 __port__)
   __u8 __inb_return = 0;
   
   __asm__ volatile(
-    "inb %%dx, %%al\n"
+    "inb %1, %0\n"
     : "=a"(__inb_return)
     : "d"(__port__)
     :
@@ -88,7 +88,7 @@ inline __attribute__((always_inline)) __u16 inw(__u16 __port__)
   __u16 __inb_return = 0;
   
   __asm__ volatile(
-    "inw %%dx, %%al\n"
+    "inw %1, %0\n"
     : "=a"(__inb_return)
     : "d"(__port__)
     :
@@ -101,7 +101,7 @@ inline __attribute__((always_inline)) __u32 inl(__u16 __port__)
   __u32 __inb_return = 0;
   
   __asm__ volatile(
-    "inl %%dx, %%al\n"
+    "inl %1, %0\n"
     : "=a"(__inb_return)
     : "d"(__port__)
     :
