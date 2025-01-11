f2ad3191cecb7e16d893ca39c838af9540970d4e
Linuxperoxo <linuxperoxo@gmail.com>
2025-01-10 22:14:30 -0300

    fix bug in task_switch function

--- arch/x86/kernel/task.c
+++ arch/x86/kernel/task.c
@@ -17,6 +17,8 @@
 #include <sys/kernel.h>
 #include <std/int.h>
 #include <std/utils.h>
+#include <std/io.h>
+#include <timer.h>
 
 #define TASK_BLOCK 0b0011
 #define TASK_TERMI 0b0010
@@ -29,17 +31,58 @@ struct
   struct Task* __root_task;
 }__TaskList;
 
+/*
+ *
+ * Toda a parte comentada foi usada para testes, caso queira usar 
+ * é so descomentar :)
+ *
+ */
+
+/*  
+
+void task1_code()
+{
+  while(1)
+  {
+    printf("Running Task1... ");
+    sleep_for(1000);
+  }
+}
+
+void task2_code()
+{
+  while(1)
+  {
+    printf("Running Task2... ");
+    sleep_for(1000);
+  }
+}
+
+*/
+
 void task_init()
 {
-  static struct Task  __task_root;
+  static struct Task  __task_1;
+  //static struct Task  __task_2;
 
-  __TaskList.__current_task = &__task_root;
-  __TaskList.__root_task    = &__task_root;
+  __TaskList.__current_task = &__task_1;
+  __TaskList.__root_task    = &__task_1;
 
-  __TaskList.__current_task->__prev_task  = NULL;
-  __TaskList.__current_task->__next_task  = NULL;
+  __TaskList.__current_task->__prev_task  = &__task_2;
+  __TaskList.__current_task->__next_task  = &__task_2;
   __TaskList.__current_task->__user_id    = 0x00;
   __TaskList.__current_task->__task_state = TASK_SLEEP; 
+  __TaskList.__current_task->__eip        = (__u32)&task1_code;
+
+  /*
+  
+  __task_2.__prev_task = &__task_1;
+  __task_2.__next_task = &__task_1;
+  __task_2.__user_id    = 0x00;
+  __task_2.__task_state = TASK_SLEEP; 
+  __task_2.__eip        = (__u32)&task2_code;
+  
+  */
 }
 
 void task_save(struct InterruptRegisters* __int_regs__)
@@ -50,7 +93,7 @@ void task_save(struct InterruptRegisters* __int_regs__)
   __TaskList.__current_task->__edx = __int_regs__->__edx;
   __TaskList.__current_task->__edi = __int_regs__->__edi;
   __TaskList.__current_task->__esi = __int_regs__->__esi;
-  __TaskList.__current_task->__eip = __int_regs__->__eip;
+ // __TaskList.__current_task->__eip = __int_regs__->__eip;
 }
 
 void task_load(struct InterruptRegisters* __int_regs__)
@@ -68,13 +111,14 @@ void task_switch(struct InterruptRegisters* __int_regs__)
 {
   task_save(__int_regs__);
 
-  while(__TaskList.__current_task->__prev_task != NULL)
+  __TaskList.__current_task->__task_state = TASK_SLEEP;
+
+  while(__TaskList.__current_task->__next_task != NULL)
   {
-    if(__TaskList.__current_task->__prev_task->__task_state == TASK_SLEEP)
-    {
-      __TaskList.__current_task = __TaskList.__current_task->__prev_task;
+    __TaskList.__current_task = __TaskList.__current_task->__next_task;
+    
+    if(__TaskList.__current_task->__task_state == TASK_SLEEP)
       break;
-    }
   }
   task_load(__int_regs__);
 }
