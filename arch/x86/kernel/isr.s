52598adfac1442fb4bee90ce101aee93296f14ae
Linuxperoxo <linuxperoxo@gmail.com>
2025-01-12 14:16:55 -0300

    rework idt.*/isr | fix interruptions registers saves

--- arch/x86/kernel/isr.s
+++ arch/x86/kernel/isr.s
@@ -6,151 +6,245 @@
 ;    |  COPYRIGHT : (c) 2024 per Linuxperoxo.     |
 ;    |  AUTHOR    : Linuxperoxo                   |
 ;    |  FILE      : isr.s                         |
-;    |  SRC MOD   : 13/12/2024                    |
+;    |  SRC MOD   : 12/01/2025                    |
 ;    |                                            |
 ;    O--------------------------------------------/
 ;
 ;
 ;
 
+[BITS 32]
+
+%define KERNEL_CODE_SEG  0x08
+%define KERNEL_DATA_SEG  0x10
+%define KERNEL_STACK_SEG 0x10
+
 extern isr_handler
-extern irq_isr_handler
+extern irq_handler
 
-%macro ISR_NOERRORCODE 1
-  global isr%1
-  
-  isr%1:
-    CLI
-    PUSH LONG 0
-    PUSH LONG %1
-    JMP isr_common_stub
-%endmacro
-
-%macro ISR_ERRORCODE 1
+%macro ISR_EXCEPTION 2
   global isr%1
 
   isr%1:
-    CLI
-    PUSH LONG %1
-    JMP isr_common_stub
+    CLI          ; No Interruptions :)
+    
+    PUSH LONG %2 ; Send Error Code to InterruptRegisters struct 
+    PUSH LONG %1 ; Interrupt Number to InterruptRegisters struct
+    
+    JMP isr_calling
 %endmacro
 
-%macro IRQ_ISR 2
-  global irq%1
+%macro ISR_IRQ 1
+  global irq%1:
 
   irq%1:
-    PUSH LONG 0
-    PUSH LONG %2
-    JMP irq_isr_common_stub
+    CLI            ; No Interruptions :)
+    
+    PUSH LONG 0x00 ; Send Error Code to InterruptRegisters struct 
+    PUSH LONG %1   ; Interrupt Number to InterruptRegisters struct
+    
+    JMP irq_calling
 %endmacro
 
-ISR_NOERRORCODE 0
-ISR_NOERRORCODE 1
-ISR_NOERRORCODE 2
-ISR_NOERRORCODE 3
-ISR_NOERRORCODE 4
-ISR_NOERRORCODE 5
-ISR_NOERRORCODE 6
-ISR_NOERRORCODE 7
-ISR_ERRORCODE 8
-ISR_NOERRORCODE 9
-ISR_ERRORCODE 10
-ISR_ERRORCODE 11
-ISR_ERRORCODE 12
-ISR_ERRORCODE 13
-ISR_ERRORCODE 14
-ISR_NOERRORCODE 15
-ISR_NOERRORCODE 16
-ISR_NOERRORCODE 17
-ISR_NOERRORCODE 18
-ISR_NOERRORCODE 19
-ISR_NOERRORCODE 20
-ISR_NOERRORCODE 21
-ISR_NOERRORCODE 22
-ISR_NOERRORCODE 23
-ISR_NOERRORCODE 24
-ISR_NOERRORCODE 25
-ISR_NOERRORCODE 26
-ISR_NOERRORCODE 27
-ISR_NOERRORCODE 28
-ISR_NOERRORCODE 29
-ISR_NOERRORCODE 30
-ISR_NOERRORCODE 31
-IRQ_ISR 0, 32
-IRQ_ISR 1, 33
-IRQ_ISR 2, 34
-IRQ_ISR 3, 35
-IRQ_ISR 4, 36
-IRQ_ISR 5, 37
-IRQ_ISR 6, 38
-IRQ_ISR 7, 39
-IRQ_ISR 8, 40
-IRQ_ISR 9, 41
-IRQ_ISR 10, 42
-IRQ_ISR 11, 43
-IRQ_ISR 12, 44
-IRQ_ISR 13, 45
-IRQ_ISR 14, 46
-IRQ_ISR 15, 47
-ISR_NOERRORCODE 128
-ISR_NOERRORCODE 177
+;
+; Ambas as routines abaixo servem para montar a
+; struct do tipo InterruptRegisters na stack, e
+; depois passar o ponteiro dessa struct montada na
+; stack para a função de handler da interrupção, se for
+; uma exception do processador vamos chamar a isr_handler, se for
+; uma request vamos chamar irq_handler, ambas recebem esse ponteiro
+;
 
-isr_common_stub:
-  PUSHA
-  MOV EAX, DS
-  PUSH EAX
-  MOV EAX, CS
+isr_calling:
+
+  ;
+  ; Montando a struct na stack
+  ;
+
+  PUSH EDI
+  PUSH ESI
+  PUSH EDX
+  PUSH ECX
+  PUSH EBX
   PUSH EAX
 
-  MOV AX, 0x10
+  MOV AX, GS
+  PUSH AX
+
+  MOV AX, ES
+  PUSH AX
+
+  MOV AX, FS
+  PUSH AX
+
+  MOV AX, SS
+  PUSH AX
+
+  MOV AX, DS
+  PUSH AX
+
+  MOV AX, KERNEL_CODE_SEG
+  MOV CS, AX
+
+  MOV AX, KERNEL_DATA_SEG
   MOV DS, AX
-  MOV ES, AX
-  MOV FS, AX
-  MOV GS, AX
+  MOV SS, AX
 
-  PUSH ESP
+  PUSH ESP ; Parâmetro para a função irq_handler(struct InterruptRegisters*)
 
   CALL isr_handler
 
-  ADD ESP, 8
-  POP EBX
-  MOV DS, BX
-  MOV ES, BX
-  MOV FS, BX
-  MOV GS, BX
+  POP ESP
+
+  ;
+  ; Agora vamos restaurar todos os registradores
+  ;
   
-  POPA
-
-  ADD ESP, 8
-  STI
-  IRET
-
-irq_isr_common_stub:
-  PUSHA
-  MOV EAX, DS
-  PUSH EAX
-  MOV EAX, CR2
-  PUSH EAX
-
-  MOV AX, 0x10
+  POP AX
   MOV DS, AX
-  MOV ES, AX
+
+  POP AX
+  MOV SS, AX
+   
+  POP AX
   MOV FS, AX
+
+  POP AX
+  MOV ES, AX
+
+  POP AX
   MOV GS, AX
 
-  PUSH ESP
-
-  CALL irq_isr_handler
-
-  ADD ESP, 8
+  POP EAX
   POP EBX
-  MOV DS, BX
-  MOV ES, BX
-  MOV FS, BX
-  MOV GS, BX
+  POP ECX
+  POP EDX
+  POP ESI
+  POP EDI
   
-  POPA
+  ADD ESP, 8
+
+  STI  ; Habilitando as interrupções novamente
+  IRET ; Aqui o processador vai desempilhar o que ele empilhou automaticamente (EIP, CS, EFLAGS)
+
+irq_calling:
+  
+  ;
+  ; Montando a struct na stack
+  ;
+  
+  PUSH EDI
+  PUSH ESI
+  PUSH EDX
+  PUSH ECX
+  PUSH EBX
+  PUSH EAX
+
+  MOV AX, GS
+  PUSH AX
+
+  MOV AX, ES
+  PUSH AX
+
+  MOV AX, FS
+  PUSH AX
+
+  MOV AX, SS
+  PUSH AX
+
+  MOV AX, DS
+  PUSH AX
+
+  PUSH ESP ; Parâmetro para a função irq_handler(struct InterruptRegisters*)
+
+  CALL irq_handler
+
+  POP ESP
+
+  ;
+  ; Agora vamos restaurar todos os registradores
+  ;
+
+  POP AX
+  MOV DS, AX
+
+  POP AX
+  MOV SS, AX
+    
+  POP AX
+  MOV FS, AX
+
+  POP AX
+  MOV ES, AX
+
+  POP AX
+  MOV GS, AX
+
+  POP EAX
+  POP EBX
+  POP ECX
+  POP EDX
+  POP ESI
+  POP EDI
 
   ADD ESP, 8
-  STI
-  IRET
+
+  STI  ; Habilitando as interrupções novamente
+  IRET ; Aqui o processador vai desempilhar o que ele empilhou automaticamente (EIP, CS, EFLAGS)
+
+ISR_EXCEPTION 0, 1 
+ISR_EXCEPTION 1, 2 
+ISR_EXCEPTION 2, 3 
+ISR_EXCEPTION 3, 4 
+ISR_EXCEPTION 4, 5 
+ISR_EXCEPTION 5, 6 
+ISR_EXCEPTION 6, 7 
+ISR_EXCEPTION 7, 8 
+ISR_EXCEPTION 8, 9 
+ISR_EXCEPTION 9, 10 
+ISR_EXCEPTION 10, 11 
+ISR_EXCEPTION 11, 12 
+ISR_EXCEPTION 12, 13 
+ISR_EXCEPTION 13, 14 
+ISR_EXCEPTION 14, 15 
+ISR_EXCEPTION 15, 16 
+ISR_EXCEPTION 16, 17 
+ISR_EXCEPTION 17, 18 
+ISR_EXCEPTION 18, 19 
+ISR_EXCEPTION 19, 20 
+ISR_EXCEPTION 20, 21 
+ISR_EXCEPTION 21, 22 
+ISR_EXCEPTION 22, 23 
+ISR_EXCEPTION 23, 24 
+ISR_EXCEPTION 24, 25 
+ISR_EXCEPTION 25, 26 
+ISR_EXCEPTION 26, 27 
+ISR_EXCEPTION 27, 28 
+ISR_EXCEPTION 28, 29 
+ISR_EXCEPTION 29, 30 
+ISR_EXCEPTION 30, 31 
+ISR_EXCEPTION 31, 32
+
+ISR_IRQ 0
+ISR_IRQ 1
+ISR_IRQ 2
+ISR_IRQ 3
+ISR_IRQ 4
+ISR_IRQ 5
+ISR_IRQ 6
+ISR_IRQ 7
+ISR_IRQ 8
+ISR_IRQ 9
+ISR_IRQ 10
+ISR_IRQ 11
+ISR_IRQ 12
+ISR_IRQ 13
+ISR_IRQ 14
+ISR_IRQ 15
+
+;
+; Syscalls
+;
+
+ISR_IRQ 128
+ISR_IRQ 177
